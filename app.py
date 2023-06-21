033e3fce4eb4648e51a96c57ba7f0f0603ddaa16
Mikołaj Glistak <mikolaj.glistak@tsh.io>
2023-06-21 23:40:03 +0200

    chore: change data fetching

--- app.py
+++ app.py
@@ -60,24 +60,27 @@ class LoginForm(FlaskForm):
 class ApiData(FlaskForm):
     def getData(fromYear, toYear, idZmienna, idPrzekroj):
         testPeroidAndPrice = []
-        testPeroidsCodes = [270, 271, 272, 273]
+        testPeroidsCodes = [247, 248, 249, 250, 251, 252, 253, 254, 255, 256, 257, 258]
         for years in range(fromYear,toYear+1):
-            for peroids in testPeroidsCodes:
+            for i, peroids in enumerate(testPeroidsCodes):
                 URL = "https://api-dbw.stat.gov.pl/api/1.1.0/variable/variable-data-section?id-zmienna="+str(idZmienna)+"&id-przekroj="+str(idPrzekroj)+"&id-rok=" + \
                 str(years) + "&id-okres=" + str(peroids) + "&ile-na-stronie=50&numer-strony=0&lang=pl"
                 string_json = requests.get(URL)
 
                 json = string_json.json()
                 data_object = json['data']
+                # To calculate average
                 countOfValuesDefferentThanZero = 0
                 valueSum = 0
                 for u in data_object:
-                    valueSum += u["wartosc"]
+                    valueSum = valueSum + u["wartosc"]
+                    print("SUMA", valueSum)
                     year = u["id-daty"]
                     peroid = u["id-okres"]
+                    print('GET ZMIENNA: ', str(idZmienna), " Okres ", u["id-okres"], " Year ", u["id-daty"])
                     if u["wartosc"] != 0: countOfValuesDefferentThanZero += 1
 
-                testPeroidAndPrice.append({'date': str(peroid % 10 + 1) + "kw. " + str(year),
+                testPeroidAndPrice.append({'date': str(i + 1) + "." + str(year),
                                            'averagePrice': round(valueSum / countOfValuesDefferentThanZero)})
         return testPeroidAndPrice
 
@@ -106,28 +109,32 @@ def home():
         toYear = 2021
     user = current_user.username
 
-    dataForAveragePriceForM2 = ApiData.getData(int(fromYear), int(toYear), 309, 485)
+    # bezrobotni nowo zarejestrowani
+    dataForAveragePriceForM2 = ApiData.getData(int(fromYear), int(toYear), 505, 16)
     labelsForAveragePriceForM2 = []
     valuesForAveragePriceForM2 = []
     for x in dataForAveragePriceForM2:
         labelsForAveragePriceForM2.append(x["date"])
         valuesForAveragePriceForM2.append(x["averagePrice"])
 
-    dataForMedianPriceForM2 = ApiData.getData(int(fromYear), int(toYear), 307, 485)
+    # bezrobotni wyrejestrowani
+    dataForMedianPriceForM2 = ApiData.getData(int(fromYear), int(toYear), 506, 16)
     labelsForMedianPriceForM2 = []
     valuesForMedianPriceForM2 = []
     for x in dataForMedianPriceForM2:
         labelsForMedianPriceForM2.append(x["date"])
         valuesForMedianPriceForM2.append(x["averagePrice"])
 
-    dataForAveragePriceForFlat = ApiData.getData(int(fromYear), int(toYear), 308, 485)
+    # Bezrobotni zarejestrowani
+    dataForAveragePriceForFlat = ApiData.getData(int(fromYear), int(toYear), 507, 16)
     labelsForAveragePriceFlat = []
     valuesForAveragePriceFlat = []
     for x in dataForAveragePriceForFlat:
         labelsForAveragePriceFlat.append(x["date"])
         valuesForAveragePriceFlat.append(x["averagePrice"])
 
-    dataForPriceFlatPointer = ApiData.getData(int(fromYear), int(toYear), 310, 484)
+    # Zmiana ogólnej sytuacji gospodarczej
+    dataForPriceFlatPointer = ApiData.getData(int(fromYear), int(toYear), 477, 16)
     labelsForAverageFlatPricesPointer = []
     valuesForAverageFlatPricesPointer = []
     for x in dataForPriceFlatPointer:
